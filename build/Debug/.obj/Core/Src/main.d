62c4c8f0f7a35e85a2f853b266bd28591a9c6953
Beyond-Stars-ai <3220852648@qq.com>
2025-12-15 17:54:50 +0800

    遥控移植现在是半成功

--- build/Debug/.obj/Core/Src/main.d
+++ build/Debug/.obj/Core/Src/main.d
@@ -30,4 +30,5 @@ build/Debug/.obj/Core/Src/main.o: Core/Src/main.c Core/Inc/main.h \
  Middlewares/Third_Party/FreeRTOS/Source/include/mpu_wrappers.h \
  Middlewares/Third_Party/FreeRTOS/Source/include/task.h \
  Middlewares/Third_Party/FreeRTOS/Source/include/list.h \
- Middlewares/Third_Party/FreeRTOS/Source/CMSIS_RTOS_V2/cmsis_os2.h
+ Middlewares/Third_Party/FreeRTOS/Source/CMSIS_RTOS_V2/cmsis_os2.h \
+ Core/Bsp/remote_control.h
